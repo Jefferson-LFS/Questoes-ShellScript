505a9e1d7c0230adcc70cf68b3bdb51d5a02ee7e
Jefferson-LFS <jefferson.altere@gmail.com>
2021-04-13 21:00:59 -0300

    add prints

--- Lista 07/questao_3.sh
+++ Lista 07/questao_3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
+echo -e " DIRS\n"
 for i in *; do  
-
   if test -d $i; then
     ls -d $i   
   fi
 done
 
+echo -e "\n LINKS\n"
 for i in *; do  
   if test -h $i; then
     ls $i
@@ -15,12 +15,11 @@ for i in *; do
 
 done
 
+echo -e " \n FILES\n"
 for i in *; do  
   if test -e $i; then
     if test -h $i; then
-     $i &> /dev/null  
-    elif test -x $i; then
-     $i &> /dev/null  
+     $i &> /dev/null 
     else
       ls $i  
     fi  
